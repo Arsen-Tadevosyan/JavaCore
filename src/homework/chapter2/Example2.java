78d3885d170b0d8a738b2f02a631d5c86a734a0e
arsen tadevosyan <arsentadevosyan370@gmail.com>
2023-08-26 02:12:53 +0300

    homework

---
+++ src/homework/chapter2/Example2.java
@@ -0,0 +1,14 @@
+package homework.chapter2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num;
+        num = 5;
+        System.out.println("num is: " + (num + num));
+        num = 100;
+        System.out.println("now num is " + (num + num));
+        num = num * 10;
+        System.out.println("num is " + (num * num));
+    }
+
+}
---
+++ src/homework/chapter2/IFSample.java
@@ -0,0 +1,14 @@
+package homework.chapter2;
+
+public class IFSample {
+    public static void main(String[] args) {
+        int x, y;
+        x = 20;
+        y = 20;
+        if (x == y) {
+          x = x * 5;
+            System.out.println(x);
+        }
+    }
+}
+
