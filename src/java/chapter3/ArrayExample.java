909e448614968e410c99c7fc8b0b44ff3f4fff49
arsen tadevosyan <arsentadevosyan370@gmail.com>
2023-08-28 21:09:01 +0300

    java

--- src/java/chapter3/ArrayExample.java
+++ src/java/chapter3/ArrayExample.java
@@ -2,7 +2,7 @@ package java.chapter3;
 
 public class ArrayExample {
     public static void main(String[] args) {
-      int month_days[]={31,28,31,30,31,30,30,31,31,30,31,30,31,};
-        System.out.println(month_days[3]);
-    }
+    int month_days[]={31,28,31,30,31,30,30,31,31,30,31,30,31,};
+    System.out.println(month_days[3]);
+}
 }
