4ee5223f09fdb610436cd08342dde5286554c37f
arsen tadevosyan <arsentadevosyan370@gmail.com>
2023-08-29 22:07:58 +0300

    java

---
+++ src/HomeWork1/FiguerPoiner3.java
@@ -0,0 +1,32 @@
+package HomeWork1;
+
+public class FiguerPoiner3 {
+    public static void main(String[] args) {
+        char a = '*';
+        System.out.println();
+        for (int j = 0; j < 1; j++) {
+            System.out.print("        " + a);
+        }
+        System.out.println();
+        System.out.print("      ");
+        for (int j = 0; j < 2; j++) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        System.out.print("    ");
+        for (int j = 0; j < 3; j++) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        System.out.print("  ");
+        for (int j = 0; j < 4; j++) {
+            System.out.print(a+" ");
+        }
+        System.out.println();
+        for (int j = 0; j < 5; j++) {
+            System.out.print(a+" ");
+        }
+
+    }
+
+}
