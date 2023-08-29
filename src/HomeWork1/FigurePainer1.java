ab95b18f644fe4594a6c850ff56dae5ad70df1ff
arsen tadevosyan <arsentadevosyan370@gmail.com>
2023-08-29 21:44:40 +0300

    java

---
+++ src/HomeWork1/FigurePainer1.java
@@ -0,0 +1,27 @@
+package HomeWork1;
+
+public class FigurePainer1 {
+    public static void main(String[] args) {
+        char a ='*';
+        for (int j = 0; j < 1; j++) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        for (int j = 0; j < 2; j++) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        for (int j = 0; j < 3; j++) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        for (int j = 0; j < 4; j++) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        for (int j = 0; j < 5; j++) {
+            System.out.print(a + " ");
+        }
+
+    }
+}
---
+++ src/work/chapter3/StrExample.java
@@ -0,0 +1,8 @@
+package work.chapter3;
+
+public class StrExample {
+    public static void main(String[] args) {
+        String club ="Barcelona";
+        System.out.println(club);
+    }
+}
