4a202ad183dbf2d09e227238cbc21fe48cac9e6d
arsen tadevosyan <arsentadevosyan370@gmail.com>
2023-09-03 21:18:03 +0300

    java

--- src/HomeWorks/ForExampls.java
+++ src/HomeWorks/ForExampls.java
@@ -10,7 +10,19 @@ public class ForExampls {
         System.out.println();
         System.out.println();
         //2
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) System.out.print(i + " ");
 
+        }
+        System.out.println();
+        System.out.println();
+        //3
+        int[] array = {2, 5, 8, 4, 9, 3, 7};
+        int a = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > a) a = array[i];
+        }
+        System.out.println(a);
     }
 }
 
