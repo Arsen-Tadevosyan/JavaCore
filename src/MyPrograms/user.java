75373c51501a1ef6dd4896da0840484636b3bde7
arsen tadevosyan <arsentadevosyan370@gmail.com>
2023-09-21 22:47:02 +0300

    java

---
+++ src/MyPrograms/user.java
@@ -0,0 +1,25 @@
+package MyPrograms;
+
+public class user {
+    String name;
+    String surename;
+    int age;
+
+    user(String na, String su, int ag) {
+        name = na;
+        surename = su;
+        age = ag;
+    }
+
+    void days(int a) {
+        a = age;
+        int d = age * 365;
+        System.out.println("օրական " + d);
+    }
+
+    void months(int a) {
+        a = age * 12;
+        System.out.println("ամսեկան " + a);
+    }
+
+}
---
+++ src/MyPrograms/userTest.java
@@ -0,0 +1,12 @@
+package MyPrograms;
+
+public class userTest {
+    public static void main(String[] args) {
+        user us = new user("Arsen", "Tadevosyan", 16);
+        System.out.println("անուն " + us.name);
+        System.out.println("ազգանուն " + us.surename);
+        System.out.println("տարիք " + us.age);
+        us.months(us.age);
+        us.days(us.age);
+    }
+}
