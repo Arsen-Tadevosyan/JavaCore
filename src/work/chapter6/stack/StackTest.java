e34d4f3d2d433baae5946ec696ced2d5bf7913a4
arsen tadevosyan <arsentadevosyan370@gmail.com>
2023-09-21 21:02:12 +0300

    java

---
+++ src/work/chapter6/stack/StackTest.java
@@ -0,0 +1,22 @@
+package work.chapter6.stack;
+
+public class StackTest {
+
+    public static void main(String[] args) {
+
+        Stack myStack = new Stack();
+        Stack myStack2=new Stack();
+        myStack.push(55);
+        myStack.push(66);
+        myStack.push(77);
+        
+        int a = myStack.pop();
+        int b = myStack.pop();
+        int l = myStack.pop();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(l);
+        System.out.println(myStack2.pop());
+    }
+}
