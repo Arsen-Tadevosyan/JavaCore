680727f6f727e5edc098e565aa37104a13932e45
arsen tadevosyan <arsentadevosyan370@gmail.com>
2023-09-28 23:22:50 +0300

    java

--- src/HomeWorks/bracechecker/BraceChackerTestV2.java
+++ src/HomeWorks/bracechecker/BraceChackerTestV2.java
@@ -1,12 +1,12 @@
 package HomeWorks.bracechecker;
 
-public class BraceChackerTest {
+public class BraceChackerTestV2 {
 
     public static void main(String[] args) {
         String text = "Hello (from) [java}";
-        BraceChecker braceChecker = new BraceChecker(text, '[', ']');
+        BraceCheckerV2 braceChecker = new BraceCheckerV2(text, '[', ']');
         braceChecker.check();
-        BraceChecker braceChecker1 = new BraceChecker(text, '(', ')');
+        BraceCheckerV2 braceChecker1 = new BraceCheckerV2(text, '(', ')');
         braceChecker1.check();
     }
 }
